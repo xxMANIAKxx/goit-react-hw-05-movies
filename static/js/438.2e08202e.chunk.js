423f97dc3c0d8b2234edde3198e6b7223d0254a3
xxMANIAKxx <99410973+xxMANIAKxx@users.noreply.github.com>
2023-05-03 08:42:26 +0000

    Deploying to gh-pages from @ xxMANIAKxx/goit-react-hw-05-movies@3733fc1bb45bbd7da1a9c5ea3d9c25ef36b66e34 🚀

---
+++ static/js/438.2e08202e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[438],{438:function(e,t,n){n.r(t),n.d(t,{NotFound:function(){return c}});var r=n(184),c=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=438.2e08202e.chunk.js.map
